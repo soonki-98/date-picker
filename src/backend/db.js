f72e29278ec41e91a7b519ef1b4fd55d5517aa6d
rugaheedori <qufslarkgml@gmail.com>
2021-07-26 15:43:08 +0900

    test

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,10 +1,8 @@
-// export const db = {
-//   id: 1,
-//   title: "hello",
-//   startDate: "2021-07-26",
-//   endDate: "2021-07-26",
-//   Completed: true,
-//   color: "blue",
-// };
-
-console.log("su");
+export const db = {
+  id: 2,
+  title: "hello",
+  startDate: "2021-07-26",
+  endDate: "2021-07-26",
+  Completed: true,
+  color: "blue",
+};
